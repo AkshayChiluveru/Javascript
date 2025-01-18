26b2d6b1857cbf1aa527b50ae56051529b9a40a0
AkshayChiluveru <87972245+AkshayChiluveru@users.noreply.github.com>
2025-01-19 05:25:53 +0530

    added the use case of includes

---
+++ includes.js
@@ -0,0 +1,10 @@
+const str = ['one', 'two', 'three'];
+const abc = 'hello';
+
+const res = str.includes('on');
+const res2 = str.includes('onetwo');
+const res3 = abc.includes('ll');
+
+console.log(res);
+console.log(res2);
+console.log(res3);
